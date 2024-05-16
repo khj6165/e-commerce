3d83c89c836ea7f4757dab6573cec6d2930ab576
khj6165 <khj6165@gmail.com>
2024-05-17 02:53:25 +0900

    - 밀린과제

--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.controller;
+
 import lombok.Getter;
 import lombok.Setter;
+
 import jakarta.validation.constraints.NotEmpty;
 
 @Getter @Setter
@@ -8,6 +10,7 @@ public class MemberForm {
 
     @NotEmpty(message = "회원 이름은 필수 입니다")
     private String name;
+
     private String city;
     private String street;
     private String zipcode;
